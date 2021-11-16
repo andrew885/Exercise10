fb8f364b5c3a28d92d8bf2fcb34023fd157cbc49
Андрей Тарашкевич <andrew.tarashkevich9@gmail.com>
2021-11-16 23:46:09 +0300

    ex10

---
+++ ClassLibrary1/MyList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class MyList<MyItem> : IList<MyItem>
+    {      
+        IList<MyItem> myList;
+        public MyList()
+        {
+            myList = new List<MyItem>();
+        }
+        public MyItem this[int index]
+        {
+            get
+            {
+                if (index <= myList.Count && index >= 0)
+                    return myList[index];
+                else
+                    return default(MyItem);
+            }
+            set
+            {
+                myList[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return myList.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return myList.IsReadOnly;
+            }            
+        }
+
+        public void Add(MyItem item)
+        {
+            myList.Add(item);
+        }
+
+        public void Clear()
+        {
+            myList.Clear();
+        }
+
+        public bool Contains(MyItem item)
+        {
+            if (item == null)
+                return false;
+            else
+                return myList.Contains(item);
+        }
+
+        public void CopyTo(MyItem[] array, int arrayIndex)
+        {
+            myList.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<MyItem> GetEnumerator()
+        {
+            return myList.GetEnumerator();
+        }
+
+        public int IndexOf(MyItem item)
+        {
+            if (item == null)
+                return -1;
+            else
+            {
+                return myList.IndexOf(item);
+            }                          
+        }
+
+        public void Insert(int index, MyItem item)
+        {
+            myList.Insert(index,item);
+        }
+
+        public bool Remove(MyItem item)
+        {
+            if (item == null)
+                return false;
+            else
+                return myList.Remove(item);           
+        }
+
+        public void RemoveAt(int index)
+        {
+            myList.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Exercise10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<MyItem> phrases = new MyList<MyItem>();
+            var t1 = new MyItem("Дратути");
+            var t2 = new MyItem("Шалом");
+            var t3 = new MyItem("Бонжур");
+            phrases.Add(t1);            
+            phrases.Add(t2);
+            phrases.Add(t3);
+
+            Console.WriteLine("Phrases contains Бонжур: "+phrases.Contains(t3));
+
+            Console.WriteLine("Phrases count: " + phrases.Count);
+
+            Console.WriteLine("Index of Бонжур: " + phrases.IndexOf(t3));
+
+            foreach (var phrase in phrases)
+            {
+                Console.WriteLine(phrase.Name);
+            }           
+        }
+    }
+}
